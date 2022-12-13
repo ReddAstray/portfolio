c89d63a67c410f96967d20dc5a9030bfe4161ca9
Fayaaz Abdoel <90307474+ReddAstray@users.noreply.github.com>
2022-12-13 13:23:10 +0100

    genshin impact

---
+++ beschrijving.php
@@ -0,0 +1,58 @@
+<!-- Begin van html -->
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title></title>
+    <!-- Style toevoegen -->
+    <style media="screen">
+      body{
+        background-image: url('afb/GenshinMain.jpg');
+        background-size: cover;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- Afbeelding van het logo van Genshin Impact -->
+    <div class="logo">
+      <a href="home.php">
+          <img src="afb/Logo.png" alt="" height="50%" width="50%">
+      </a>
+    </div>
+
+<!-- Text box 1 -->
+    <div class="box1">
+<p>Genshin Impact is een rpg game gemaakt door het bedrijf
+  <br>
+  Mihoyo in 2020. Het is een anime style open-world game
+  <br>
+  met een actiebased battlesysteem waarin je magie kan gebruiken.
+  <br>
+  De spelers kunnen nieuwe characters en wapens
+  <br>
+  krijgen door een gacha mechanisme.</p>
+    </div>
+<!-- Text box 2 -->
+    <div class="box2">
+    <p>Genshin Impact vind plaats in een fantasie wereld <br>
+      genaamd Teyvat, waarin 7 verschillende natie's liggen. <br>
+      Elke natie met een eigen element (magie) <br>
+      en een eigen god. <br><br>
+      De Traveler heeft veel werelden bezocht met zijn <br>
+      tweelingzus voordat ze in Teyvat terecht kwamen. <br>
+      Toen ze eenmaal in Teyvat waren zijn ze elkaar kwijt <br>
+      geraakt. <br>
+      In het verhaal is de Traveler opzoek naar zijn <br>
+      tweelingzus en komt hierdoor met verschillende <br>
+      dingen te maken.</p>
+    </div>
+    <!-- Text box 3 -->
+    <div class="box3">
+      <p>Genshin Impact is verkrijgbaar op alle platformen <br>
+        behalve op de Nintendo Switch, dit zijn ze nog van plan. <br>
+        De game heeft meer dan $3 billion gehaald in het <br>
+        eerste jaar, het hoogste bedrag van alle video-games <br>
+        in het eerste jaar ooit.</p>
+    </div>
+  </body>
+</html>
